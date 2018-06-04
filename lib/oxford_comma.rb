2704fc326862d26f844c0c484dfff9615771af00
Jed Peek <jedpeek@yahoo.com>
2018-06-04 20:46:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
 if array != array[-1]
   string = array.join(", ")
-else
-  array.push(" and #{array[-1]})
+end
+  string = string.push(" and #{array[-1]})
 end
 return string
 end
