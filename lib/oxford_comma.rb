1cb0985fd6be834d5c0f9bb549897e8d3554f433
Jed Peek <jedpeek@yahoo.com>
2018-06-04 20:46:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array != array[-1]
   string = array.join(", ")
 end
-  string = string.push(" and #{array[-1]})
+  string = string.push(" and #{array[-1]}")
 end
 return string
 end
