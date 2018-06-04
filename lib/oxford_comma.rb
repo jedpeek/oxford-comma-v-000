46783d839ff8bdf45e9f43d289eb125fc0886b84
Jed Peek <jedpeek@yahoo.com>
2018-06-04 20:52:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
+  if array.size == 1
+    string = array.to_s
+elsif array.size < 3
+    string = array.join(" and ")
 array[-1] = "and #{array[-1]}"
 string = array.join(", ")
 return string
